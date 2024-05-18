5a50601ed9d169f8ca4970201560d7f0005336d4
Saksham <atomixdes@icloud.com>
2024-05-17 21:21:06 -0400

    Implement playRound function.

--- script.js
+++ script.js
@@ -23,7 +23,46 @@ function getHumanChoice() {
     return prompt("Choose Rock | Paper | Scissors: ");
 }
 
-let computer = getComputerChoice();
-console.log(computer);
-let human = getHumanChoice();
-console.log(human);
+function playRound(computerChoice, humanChoice) {
+    computerChoice = computerChoice.toUpperCase();
+    humanChoice = humanChoice.toUpperCase();
+
+    if (computerChoice === humanChoice) {
+        return "Tie";
+    }
+
+    if (computerChoice === "ROCK" && humanChoice === "SCISSORS") {
+        computerScore++;
+        return "You lose! Rock beats Scissors!";
+    }
+    else if (computerChoice === "SCISSORS" && humanChoice === "ROCK") {
+        humanScore++;
+        return "You win! Rock beats Scissors!";
+    }
+    else if (computerChoice === "PAPER" && humanChoice === "ROCK") {
+        return "You lose! Paper beats Rock!";
+    }
+    else if (computerChoice === "ROCK" && humanChoice === "PAPER") {
+        humanScore++;
+        return "You win! Paper beats Rock!"
+    }
+    else if (computerChoice === "SCISSORS" && humanChoice === "PAPER") {
+        computerChoice++;
+        return "You lose! Scissors beats Paper!";
+    }
+    else if (computerChoice === "PAPER" && humanChoice === "SCISSORS") {
+        return "You win! Scissors beats Paper!";
+    }
+
+    return "Error";
+}
+
+let computerResponse = getComputerChoice();
+console.log(computerResponse);
+let humanResponse = getHumanChoice();
+console.log(humanResponse);
+
+let computerScore = 0;
+let humanScore = 0;
+
+console.log(playRound(computerResponse, humanResponse));
