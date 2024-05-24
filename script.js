558c487e6168e4e692b92c9c3807ab895700bc79
Saksham <atomixdes@icloud.com>
2024-05-24 19:06:59 -0400

    Overhaul game to support UI.

--- script.js
+++ script.js
@@ -22,78 +22,90 @@ function getComputerChoice() {
     return toReturn;
 }
 
-function getHumanChoice() {
-    return prompt("Choose Rock | Paper | Scissors: ");
-}
-
-function playGame() {
-    function playRound(computerChoice, humanChoice) {
-        
-        computerChoice = computerChoice.toUpperCase();
-        if (humanChoice != null) {
-            humanChoice = humanChoice.toUpperCase();
-        }
-
-        if (computerChoice === humanChoice) {
-            return "Tie";
-        }
-
-        if (computerChoice === "ROCK" && humanChoice === "SCISSORS") {
-            computerScore++;
-            return "You lose! Rock beats Scissors!";
-        }
-        else if (computerChoice === "SCISSORS" && humanChoice === "ROCK") {
-            humanScore++;
-            return "You win! Rock beats Scissors!";
-        }
-        else if (computerChoice === "PAPER" && humanChoice === "ROCK") {
-            computerScore++;
-            return "You lose! Paper beats Rock!";
-        }
-        else if (computerChoice === "ROCK" && humanChoice === "PAPER") {
-            humanScore++;
-            return "You win! Paper beats Rock!"
-        }
-        else if (computerChoice === "SCISSORS" && humanChoice === "PAPER") {
-            computerScore++;
-            return "You lose! Scissors beats Paper!";
-        }
-        else if (computerChoice === "PAPER" && humanChoice === "SCISSORS") {
-            humanScore++;
-            return "You win! Scissors beats Paper!";
-        }
-
-        return "Error";
+function playRound(computerChoice, humanChoice) {
+    
+    computerChoice = computerChoice.toUpperCase();
+    if (humanChoice != null) {
+        humanChoice = humanChoice.toUpperCase();
     }
 
-    let toReturn = "Error";
-    let i = 1;
-    while (toReturn === "Error") {
-        console.log("Round: " + (i));
-
-        let computerResponse = getComputerChoice();
-        let humanResponse = getHumanChoice();
-
-        console.log(playRound(computerResponse, humanResponse));
-
-        if (computerScore === 3) {
-            toReturn = "Computer Wins, Try again!";
-            computerScore = 0;
-            humanScore = 0;
-            break;
-        }
-
-        if (humanScore === 3) {
-            toReturn = "You Won the Game!";
-            computerScore = 0;
-            humanScore = 0;
-            break;
-        }
-
-        i++;
+    if (computerChoice === humanChoice) {
+        return "Tie";
     }
 
-    return toReturn;
+    if (computerChoice === "ROCK" && humanChoice === "SCISSORS") {
+        computerScore++;
+        return "You lose! Rock beats Scissors!";
+    }
+    else if (computerChoice === "SCISSORS" && humanChoice === "ROCK") {
+        humanScore++;
+        return "You win! Rock beats Scissors!";
+    }
+    else if (computerChoice === "PAPER" && humanChoice === "ROCK") {
+        computerScore++;
+        return "You lose! Paper beats Rock!";
+    }
+    else if (computerChoice === "ROCK" && humanChoice === "PAPER") {
+        humanScore++;
+        return "You win! Paper beats Rock!"
+    }
+    else if (computerChoice === "SCISSORS" && humanChoice === "PAPER") {
+        computerScore++;
+        return "You lose! Scissors beats Paper!";
+    }
+    else if (computerChoice === "PAPER" && humanChoice === "SCISSORS") {
+        humanScore++;
+        return "You win! Scissors beats Paper!";
+    }
+
+    return "Error";
 }
 
-console.log(playGame());
+function checkGame(humanResponse) {
+    console.log("Round: " + (i));
+
+    let computerResponse = getComputerChoice();
+
+    console.log(playRound(computerResponse, humanResponse));
+
+    if (computerScore === 5) {
+        winnerText = "Computer Wins, Try again!";
+        computerScore = 0;
+        humanScore = 0;
+        return true;
+    }
+
+    if (humanScore === 5) {
+        winnerText = "You Won the Game!";
+        computerScore = 0;
+        humanScore = 0;
+        return true;
+    }
+
+    i++;
+
+    return false;
+}
+
+let i = 1;
+let winnerText = "Error";
+console.log("=====================================");
+
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        console.log(button.textContent + " was pressed!");
+        if (checkGame(button.textContent) === true) {
+            console.log(winnerText);
+            console.log("Game Over!");
+            i = 1;
+            toReturn = "Error";
+        }
+        else {
+            console.log("Your Score: " + humanScore);
+            console.log("Computer Score: " + computerScore);
+        }
+        console.log("=====================================");
+    })
+})
